10197871363148ec91a92979cef6ec70ad058ac3
Oliver Tilk <olivertilk@gmail.com>
2019-11-24 22:43:35 -0800

    Fixed code style

--- client/client.js
+++ client/client.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 const grpc = require('grpc');
 
 const greet = require('../server/protos/greet_pb');
@@ -5,24 +6,24 @@ const service = require('../server/protos/greet_grpc_pb');
 
 function main() {
     console.log('Hello from client');
-    
+
     const client = new service.GreetingServiceClient('localhost:50051', grpc.credentials.createInsecure());
 
-    //Set up the RPC request
-    let request = new greet.GreetingRequest();
+    // Set up the RPC request
+    const request = new greet.GreetingRequest();
 
-    //The request is sent with a message of type Greeting, so set it up
-    let greeting = new greet.Greeting();
-    greeting.setFirstName("Jerry");
-    greeting.setLastName("Tom");
+    // The request is sent with a message of type Greeting, so set it up
+    const greeting = new greet.Greeting();
+    greeting.setFirstName('Jerry');
+    greeting.setLastName('Tom');
     request.setGreeting(greeting);
 
-    //Make the API call
+    // Make the API call
     client.greet(request, (error, response) => {
-        if(!error) {
-            console.log("Greeting response", response.getResult());
+        if (!error) {
+            console.log('Greeting response', response.getResult());
         } else {
-            console.log("Error", error);
+            console.log('Error', error);
         }
     });
 }
--- server/server.js
+++ server/server.js
@@ -1,36 +1,36 @@
+/* eslint-disable no-console */
 const grpc = require('grpc');
 
 const greets = require('../server/protos/greet_pb');
 const service = require('../server/protos/greet_grpc_pb');
 
-//Implement the greet RPC method
+// Implement the greet RPC method
 function greet(call, callback) {
-
-    //Set up the response object
+    // Set up the response object
     const greeting = new greets.GreetingResponse();
 
-    //Set the 'result' field of the response object to say hello
-    //Get the first name of the request object from the 'call' variable
-    greeting.setResult("Hello " + call.request.getGreeting().getFirstName());
-    console.log("call:", call);
-    console.log("call request:", call.request);
-    
-    //The first argument to the callback is for reporting errors
+    // Set the 'result' field of the response object to say hello
+    // Get the first name of the request object from the 'call' variable
+    const firstName = call.request.getGreeting().getFirstName();
+    const lastName = call.request.getGreeting().getLastName();
+    greeting.setResult(`Hello ${firstName} ${lastName}`);
+
+    // The first argument to the callback is for reporting errors
     callback(null, greeting);
 }
 
 function main() {
-    //Start gRPC server to listen to requests
+    // Start gRPC server to listen to requests
     const server = new grpc.Server();
 
-    //Specify which API endpoints (RPC method) from the service to add
-    server.addService(service.GreetingServiceService, {greet: greet});
+    // Specify which API endpoints (RPC method) to create
+    server.addService(service.GreetingServiceService, { greet });
 
-    //Don't add http:// in front of localhost as it may cause errors
-    server.bind("localhost:50051", grpc.ServerCredentials.createInsecure());
+    // Don't add http:// in front of localhost as it may cause errors
+    server.bind('localhost:50051', grpc.ServerCredentials.createInsecure());
     server.start();
 
-    console.log("Server running on port localhost:50051");
+    console.log('Server running on port localhost:50051');
 }
 
 main();
